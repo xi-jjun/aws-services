9000c58946d6e2a503c461e73412eb847153e78d
xi_jjun <rlawowns97@naver.com>
2022-10-28 00:56:26 +0900

    aws ecs fargate init

---
+++ ecsFargateDemo/src/main/java/me/practice/awsecsfargate/EcsFargateDemoApplication.java
@@ -0,0 +1,13 @@
+package me.practice.awsecsfargate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsFargateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsFargateDemoApplication.class, args);
+	}
+
+}
---
+++ ecsFargateDemo/src/test/java/me/practice/awsecsfargate/EcsFargateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.practice.awsecsfargate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsFargateDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
